59cf8c8dca822d6d02f5707c0e2c88f3a863dad1
Natália Condê <natalia@back4app.com>
2022-12-28 16:14:11 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
